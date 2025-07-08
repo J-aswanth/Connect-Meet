aa10f8fff89882f1cae92b5910381ab5907fc139
J-aswanth <128897545+J-aswanth@users.noreply.github.com>
2025-07-09 00:52:56 +0530

    update

---
+++ auth.js
@@ -0,0 +1,24 @@
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth2').Strategy;
+
+const GOOGLE_CLIENT_ID ='645963397923-9ci0b3pkc9mudc649oe8c8vmk3c295vb.apps.googleusercontent.com';
+const GOOGLE_CLIENT_SECRET = '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';
+
+passport.use(new GoogleStrategy({
+  clientID: GOOGLE_CLIENT_ID,
+  clientSecret: GOOGLE_CLIENT_SECRET,
+  callbackURL: "http://localhost:5001/google/callback",
+  passReqToCallback: true,
+},
+
+function(request, accessToken, refreshToken, profile, done) {
+  return done(null, profile);
+}));
+
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function(user, done) {
+  done(null, user);
+});
